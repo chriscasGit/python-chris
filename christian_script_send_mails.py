de6533fdc868ec7dc4c9d040d1344abef87ae330
Chris <93075193+chriscasGit@users.noreply.github.com>
2021-11-02 17:44:00 +0100

    Update christian_script_send_mails.py

--- christian_script_send_mails.py
+++ christian_script_send_mails.py
@@ -1,3 +1,4 @@
+#Christian Castaneda - script send emails v4 - 02 Nov 2021
 #import libraries
 import win32com.client as win32
 import os
@@ -5,12 +6,15 @@ from pandas import *
 import sys
 import time
 
+print("Script started...")
+
 #create system variables
 t = time.localtime()
 timestamp = time.strftime('%Y%B%d_%H%M%S', t)
 cwd = os.getcwd()
 outlook = win32.Dispatch('outlook.application')
-sys.stdout = open(cwd+"\\"+"_log_"+timestamp+".txt", 'w')
+logfile = cwd+"\\"+"_log_"+timestamp+".txt"
+sys.stdout = open(logfile, 'w')
 
 #import excel file
 path = cwd+"\\"+"_email_setup.xlsx"
@@ -30,16 +34,24 @@ for index, row in df.iterrows():
         #checks if there is more than one attachment. always separate by ; or will not recognize more files
         if ";" in row["email_attachment_name"]:
             row["email_attachment_split"] = row["email_attachment_name"].split(';')
+            
             for i in row["email_attachment_split"]:
                 attachment = cwd+"\\"+i.strip()
                 mail.Attachments.Add(attachment)
+            
+            print("OK partner done (>1 attachments): " + row["partner_name"])
                            
         else:
-            attachment = cwd+"\\"+row["email_attachment_name"]
-            mail.Attachments.Add(attachment)
-        print("OK partner done: " + row["partner_name"])
+            if "NONE" in row["email_attachment_name"]:
+                 print("OK partner done (no attachments): " + row["partner_name"])
+            else:
+                attachment = cwd+"\\"+row["email_attachment_name"]
+                mail.Attachments.Add(attachment)
+                print("OK partner done (1 attachment): " + row["partner_name"])
         
     except:
         print("CHECK ERROR partner: "+row["partner_name"])
 
 sys.stdout.close()
+
+os.startfile(logfile)
